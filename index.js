75c7a9f164971d1af61c2343df69ab9eb2912f5a
blueberry20 <olgasales@gmail.com>
2018-06-04 12:48:56 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res)=>{
+	res.send({hi: 'there2'});
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000);
