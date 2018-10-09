8d073b0d1ea1e497765537d4496bf65c4afe0fd5
blueberry20 <olgasales@gmail.com>
2018-10-08 21:42:58 -0400

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys.js");
+//now keys is an object that has googleClientID and googleClientSecret properties
 
-app.get('/', (req, res)=>{
-	res.send({hi: 'there2'});
-});
+// app.get('/', (req, res)=>{
+// 	res.send({bye: 'there'});
+// });
+
+//clientID 863689672673-sktuo370oq15638qmi1fg8del7v25vp7.apps.googleusercontent.com
+//"client_secret":"3PatMzg72Wms69KN8pe819j-"
+
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
-
 
 app.listen(PORT);
